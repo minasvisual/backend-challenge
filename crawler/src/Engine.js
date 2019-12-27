3ae1e6eecbfe54225e1f96b8af01ade89e6f9950
Ulisses MAntovani <contato@minasvisual.com>
2019-12-27 04:14:38 +0000

    engine estruture creation

--- crawler/src/Engine.js
+++ crawler/src/Engine.js
@@ -50,32 +50,34 @@ module.exports = () =>
       return Array.isArray(arrData) && arrData.filter( t => {
          ext = _.get( t.href.match(/\.[0-9a-z]+$/i), '[0]', '').replace('.', '')
          return accept.indexOf(ext.toLowerCase()) > -1
-      })
+      }).map( x => x.href)
     }
     
     let validatePage = (pageArr) => {
-        return pageArr.filter( p => new RegExp(/^(?:[a-z]+:)?\/\//i).test(p.href) === false && pagesList.indexOf(p.href) === -1 )
+        return pageArr.filter( p => new RegExp(/^(?:[a-z]+:)?\/\//i).test(p.href) === false )
+                      .map(x => ( pagesList.indexOf(x.href) === -1 ? pagesList.push(x.href) : '') )
     }
    
     let getPageAssets = ({data, response}) => {
-        pagesTree[ _.get(response, 'requestUrl') ] = {
+        pagesTree[ _.get(response, 'responseUrl') ] = {
            css: validadeAsset(['css'], data.css),
            js: validadeAsset(['js'], data.js),
            images: validadeAsset(['jpg','gif','svg','png','bitmap','canvas'], data.images),
         }
       
-        return {data, pagesTree, pagesList}
+        return pagesTree
     }
     
     let getPageData = (url, objSchema) =>{
         // Promise interface
         return scrapeIt(url, objSchema)
                  .then((data) =>{
-                     pagesList = validatePage( data.data.urls )
+                     console.log(data.data)
+                     validatePage( data.data.urls )
                      return data;
                  })
                  .then( getPageAssets )
-                 .then( console.log )
+                 .then( (x) => console.log( x, pagesList ) )
     }
    
     let getPages = (mainUrl) => {
