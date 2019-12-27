3ce1a214e7217a43e18d06825b370b37b7dd408c
Ulisses MAntovani <contato@minasvisual.com>
2019-12-27 02:37:15 +0000

    engine estruture creation

--- crawler/src/Engine.js
+++ crawler/src/Engine.js
@@ -2,4 +2,25 @@ const scrapeIt = require("scrape-it")
 
 module.exports = () => 
 {
+    let pagesList,
+        pagesTree
+  
+    let getPageData = (url, schema) =>{
+        // Promise interface
+        return scrapeIt(url, {
+          
+        })
+    }
+    
+    let getPages = (mainUrl) => {
+       
+    }
+    
+    let getPageAssets = () => {
+      
+    }
+    
+    return {
+       
+    };
 }
--- crawler/src/Routes.js
+++ crawler/src/Routes.js
@@ -2,8 +2,13 @@ const express = require('express')
 
 const Router = express.Router()
 
+const engine = require('./Engine')()
+
 module.exports = () =>
 {
+    Router.post('/api/getAssets', (req, res) => {
+        
+    })
     
     return Router;
 }
